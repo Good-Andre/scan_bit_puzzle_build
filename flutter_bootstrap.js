e82e0fce810972878eda8f72b3d2c613bf398534
good_andre <good-andre@yandex.ru>
2026-09-04 11:23:07 +0300

    Deploy production build via SSH

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2111127681" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3152326724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
